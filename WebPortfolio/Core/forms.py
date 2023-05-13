2d371f0eefd80b91ccee48c0d1e03fe9095e3b79
Cristian Alvarez Toscano <Cristian_AAT@outlook.com>
2023-05-13 09:55:21 -0600

    Actualizacion de portafolio 2023

--- WebPortfolio/Core/forms.py
+++ WebPortfolio/Core/forms.py
@@ -4,25 +4,25 @@ class ContactForm(forms.Form):
     name = forms.CharField(label='Nombre', required=True, min_length=3, max_length=20, widget=forms.TextInput(
         attrs={
             'class':'form-control',
-            'placeholder':'Escribe tu nombre'
+            'placeholder':'Your name'
         }
     ))
     email = forms.EmailField(label='Correo', required=True, min_length=3, max_length=80, widget=forms.EmailInput(
         attrs={
             'class':'form-control',
-            'placeholder':'Tu correo'
+            'placeholder':'Your Mail'
         }
     ))
     phoneNumber = forms.CharField(label='Número', required=True, min_length=3, max_length=30, widget=forms.TextInput(
         attrs={
             'class':'form-control',
-            'placeholder':'Tu numero de telefono'
+            'placeholder':'Your phone number'
         }
     ))
     message = forms.CharField(label='Mensaje', required=True, min_length=10, max_length=300, widget=forms.Textarea(
         attrs={
             'class':'form-control',
-            'placeholder':'Mensaje',
+            'placeholder':'Massage',
             'rows':3
         }
     ))
